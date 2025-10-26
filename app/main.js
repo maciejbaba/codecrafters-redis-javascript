8d48b3a9ed76cfd6a3708413890906cfdb77f7f9
maciejbaba <maciek2baba2@gmail.com>
2025-10-26 12:25:31 +0100

    test

--- app/main.js
+++ app/main.js
@@ -5,8 +5,14 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment the code below to pass the first stage
 const server = net.createServer((connection) => {
-  connection.on("data", () => {
-    connection.write("+PONG\r\n");
+  connection.on("data", (data) => {
+    if (data.toString() === "PING\r\n") {
+      connection.write("+PONG\r\n");
+    } else if (data.toString().startsWith("ECHO ")) {
+      connection.write(`+${data.toString().split(" ")[1]}\r\n`);
+    } else {
+      connection.write("-ERR unknown command 'PING'\r\n");
+    }
   });
 });
 server.listen(6379, "127.0.0.1");
