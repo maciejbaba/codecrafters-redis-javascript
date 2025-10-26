ef79a14f4e1b836cbacab925bc796f65f386de4c
maciejbaba <maciek2baba2@gmail.com>
2025-10-26 12:41:38 +0100

    test

--- app/main.js
+++ app/main.js
@@ -5,6 +5,7 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment the code below to pass the first stage
 const server = net.createServer((connection) => {
+  const state = {};
   connection.on("data", (data) => {
     const commands = data.toString().split("\r\n");
     const command = commands[2];
@@ -13,6 +14,19 @@ const server = net.createServer((connection) => {
     } else if (command === "ECHO") {
       const message = commands[4];
       connection.write(`+${message}\r\n`);
+    } else if (command === "SET") {
+      const key = commands[4];
+      const value = commands[6];
+      state[key] = value;
+      connection.write(`+OK\r\n`);
+    } else if (command === "GET") {
+      const key = commands[4];
+      const value = store[key];
+      if (value) {
+        connection.write(`+${value}\r\n`);
+      } else {
+        connection.write(`$-1\r\n`);
+      }
     } else {
       connection.write("-ERR unknown command 'PING'\r\n");
     }
