9f6951f73fb746f8cbfacb3bdf937bb29730fce4
maciejbaba <maciek2baba2@gmail.com>
2025-10-26 12:19:23 +0100

    test

--- app/main.js
+++ app/main.js
@@ -5,7 +5,8 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment the code below to pass the first stage
 const server = net.createServer((connection) => {
-  // Handle connection
+  connection.on("data", () => {
+    connection.write("+PONG\r\n");
+  });
 });
-//
 server.listen(6379, "127.0.0.1");
